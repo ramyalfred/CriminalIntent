e282662cbf346dc034b06fc354b63346eb28b8bc
ramyalfred <ramy.alfred92@gmail.com>
2018-07-28 22:55:14 +0200

    Initial Commit

---
+++ app/src/main/java/com/bignerdranch/android/criminalintent/Crime.java
@@ -0,0 +1,49 @@
+package com.bignerdranch.android.criminalintent;
+
+import java.util.Date;
+import java.util.UUID;
+
+/**
+ * Created by Ramy on 7/24/2018.
+ */
+
+public class Crime {
+    private UUID mId;
+    private String mTitle;
+    private Date mDate;
+    private Boolean mSolved;
+
+    public Crime(){
+        //Generate Unique ID
+        mId = UUID.randomUUID();
+        mDate = new Date();
+    }
+
+    public UUID getId() {
+        return mId;
+    }
+
+    public String getTitle() {
+        return mTitle;
+    }
+
+    public void setTitle(String mTitle) {
+        this.mTitle = mTitle;
+    }
+
+    public Date getDate() {
+        return mDate;
+    }
+
+    public void setDate(Date mDate) {
+        this.mDate = mDate;
+    }
+
+    public Boolean isSolved() {
+        return mSolved;
+    }
+
+    public void setSolved(Boolean mSolved) {
+        this.mSolved = mSolved;
+    }
+}
